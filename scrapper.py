fbfd2d10f50f19b5d1a2b3b6677d770c62acd2c3
vcheng1 <50505766+vcheng1@users.noreply.github.com>
2019-05-12 14:11:38 -0700

    Add files via upload

--- scrapper.py
+++ scrapper.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-# Download IMDB's Top 250 data
 url = 'http://www.imdb.com/chart/top'
 url1 = 'https://www.fandango.com/91006_movietimes?mode=general&q=91006'
 response = requests.get(url)
@@ -19,9 +18,7 @@ votes = [b.attrs.get('data-value') for b in soup.select('td.ratingColumn strong'
 
 imdb = []
 
-# Store each item into dictionary (data), then put those into a list (imdb)
 for index in range(0, len(movies)):
-    # Seperate movie into: 'place', 'title', 'year'
     movie_string = movies[index].get_text()
     movie = (' '.join(movie_string.split()).replace('.', ''))
     movie_title = movie[len(str(index))+1:-7]
